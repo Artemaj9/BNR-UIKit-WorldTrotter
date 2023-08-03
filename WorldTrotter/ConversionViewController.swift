6c41177fb15b0225280c32b5f428f088e1e64405
Artem <moiseev@physics.msu.ru>
2023-08-03 22:44:03 +0300

    Two vc created

---
+++ WorldTrotter/ConversionViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  WorldTrotter
+//
+//  Created by Artem on 02.08.2023.
+//
+
+import UIKit
+
+class ConversionViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    
+    //
+    //    override func viewWillLayoutSubviews() {
+    //        super.viewWillLayoutSubviews()
+    //
+    //        let gradientLayer = CAGradientLayer()
+    //        gradientLayer.colors = [UIColor.red.cgColor,
+    //                                UIColor.yellow.cgColor,
+    //                                UIColor.green.cgColor,
+    //                                UIColor.blue.cgColor, UIColor.purple.cgColor]
+    //        gradientLayer.startPoint = CGPoint(x: 0.0, y: 0)
+    //        gradientLayer.endPoint = CGPoint(x: 0, y: 1)
+    //        gradientLayer.locations = [0.5, 0.6, 0.65, 0.67, 0.7]
+    //        gradientLayer.frame = view.bounds
+    //        view.layer.addSublayer(gradientLayer)
+    //
+    //    }
+    //}
+    
+}
---
+++ WorldTrotter/MapViewController.swift
@@ -0,0 +1,12 @@
+//
+//  MapViewController.swift
+//  WorldTrotter
+//
+//  Created by Artem on 03.08.2023.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+    
+}
